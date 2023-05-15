4de248f7bcdaf027ff19f7ca5c80e2373e4e48c4
Bartosz <jasykbartosz@gmail.com>
2023-05-15 20:22:48 +0200

    start

---
+++ cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {},
+
+    baseUrl: 'https://www.sowe.pl/',
+    includeShadowDom: true,
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    video: false,
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,feature}',
+    retries: {
+      runMode: 0,
+      openMode: 0,
+    },
+  },
+});
