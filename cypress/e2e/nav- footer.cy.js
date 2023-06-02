c1d2563fe0c6dbb135c639344ffc9edb4dd9b515
Bartosz <jasykbartosz@gmail.com>
2023-06-02 15:48:51 +0200

    main nav all links

--- cypress/e2e/nav- footer.cy.js
+++ cypress/e2e/nav- footer.cy.js
@@ -12,11 +12,11 @@ describe('Sowe - home page - E2E', () => {
   });
 
   it.only('should check all main kategory "SKLEP" links and verify correct url of first product', () => {
-    cy.contains('SKLEP').should('be.visible').click();
-    cy.url().should('eq', url.sklepUrl);
-    cy.go('back');
-    cy.checkLinksUrlsTitles('.mega-sub-menu.sub-menu>ul>li>a');
-    cy.get('.post.type-product').find('.post-image').eq(0).click();
+    // cy.contains('SKLEP').should('be.visible').click();
+    // cy.url().should('eq', url.sklepUrl);
+    // cy.go('back');
+    // cy.checkLinksUrlsTitles('.mega-sub-menu.sub-menu>ul>li>a');
+    cy.get('.mega-menu-posts.tf_left.tf_box').find('.figure.post-image').eq(0).click();
     cy.get('h1').then(($title) => {
       const title = $title.text();
       cy.get('h1').should('have.text', title);
@@ -24,7 +24,7 @@ describe('Sowe - home page - E2E', () => {
   });
 
   it('Check all links in main nav"', () => {
-    cy.checkLinksUrlsTitles('.mega-sub-menu.sub-menu>ul>li>a');
+    cy.checkLinksUrlsTitles('.mega-sub-menu.sub-menu a');
     cy.requestCheckLink('.mega-sub-menu.sub-menu>ul>li>a');
     // cy.checkLinksUrlsTitles('#main-nav a[href]');
   });
