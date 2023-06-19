97b18094869018c9c74e75c065baa1237710185e
Bartosz <jasykbartosz@gmail.com>
2023-06-19 10:31:42 +0200

    cypress cloud connect

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'pdxgjf',
   e2e: {
     setupNodeEvents(on, config) {},
 
     baseUrl: 'https://www.sowe.pl/',
-    experimentalRunAllSpecs: true,
+    experimentalRunAllSpecs: false,
     includeShadowDom: true,
     viewportHeight: 1080,
     viewportWidth: 1920,
     hideXHRInCommandLog: true,
     video: false,
+    videoUploadOnPasses: false,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,feature}',
     retries: {
       runMode: 4,
