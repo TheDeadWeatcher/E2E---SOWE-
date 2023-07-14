1dcd3a1e5b5a157330454101ff8164fcdd117014
Bartosz <jasykbartosz@gmail.com>
2023-07-14 16:25:40 +0200

    allure

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require('cypress');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   projectId: 'pdxgjf',
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      allureWriter(on, config);
+    },
 
     baseUrl: 'https://www.sowe.pl/',
     experimentalRunAllSpecs: false,
